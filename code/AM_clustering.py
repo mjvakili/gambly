f92d2d70ce1116aa564a18ac19835d6999699344
mjvakili <mjvakili@nyu.edu>
2016-05-20 21:41:49 +0000

    modified jackknife calculation

--- code/AM_clustering.py
+++ code/AM_clustering.py
@@ -22,7 +22,7 @@ def impose_luminosity_threshold(Mr):
     
     cat = read_catalog()
     luminosity = cat[:,9]
-    reduced_cat = cat[luminosity>-1.*Mr]
+    reduced_cat = cat[luminosity<-1.*Mr]
 
     return reduced_cat
 
@@ -31,7 +31,7 @@ def save_luminosity_threshold(Mr):
        luminosity threshold'''
 
     reduced_cat = impose_luminosity_threshold(Mr)
-    filename = util.dat_dir+'AM_Mr'+str(Mr)+'dat'
+    filename = util.dat_dir()+'AM_Mr'+str(Mr)+'dat'
     np.savetxt(filename , reduced_cat)
 
     return None
@@ -49,21 +49,23 @@ def measure_nbar_clustering(Mr):
                         "../", "bin")
     autocorr = 1
 
-    filename = util.dat_dir+'AM_Mr'+str(Mr)+'dat'
+    filename = util.dat_dir()+'AM_Mr'+str(Mr)+'dat'
     cat = np.loadtxt(filename)
-    x, y, z = cat[:,1:4]
-    vx, vy, vz = cat[:,4:7]
+    pos = cat[:,1:4]
+    x, y, z = pos[:,0], pos[:,1], pos[:,2] 
+    vel = cat[:,4:7]
+    vx, vy, vz =  vel[:,0], vel[:,1], vel[:,2] 
     #applying RSD:
     pos = return_xyz_formatted_array(x, y, z, velocity = vz, velocity_distortion_dimension = 'z')
     #enforcing periodic boundary conditions:
-    pos = enforce_periodicity_of_box(pos, self.boxsize)
+    pos = enforce_periodicity_of_box(pos, boxsize)
     pos = pos.astype(np.float32)
     x, y, z = pos[:,0] , pos[:,1] , pos[:,2]
     wp_result = _countpairs.countpairs_wp(boxsize, pimax, 
                                    nthreads, binfile, 
 				   x, y, z)
     nbar = 1.*len(pos)/boxsize**3.
-    wp = np.array(results_wp)[:,3]
+    wp = np.array(wp_result)[:,3]
 
     return nbar , wp    
     
@@ -74,7 +76,7 @@ def save_nbar_clustering(Mr):
     wp_filename = util.dat_dir()+'wp_AM_Mr'+str(Mr)+'.dat'
     nbar_filename = util.dat_dir()+'nbar_AM_Mr'+str(Mr)+'.dat'
     nbar , wp = measure_nbar_clustering(Mr)
-    np.savetxt(nbar_filename ,  nbar)
+    np.savetxt(nbar_filename ,  np.array([nbar]))
     np.savetxt(wp_filename ,  wp)
 
     return None
@@ -103,9 +105,26 @@ def edge(index , nsub):
     return xi , yi , zi
     
          
-def mask_galaxy_table(pos , subvol_index , nsub):
-    '''masks the positions of galaxies
+def mask_catalog(cat , subvol_index , nsub):
+    '''masks the AM catalogs
     '''
+    box_size = 250.
+    subbox_size = 1.*box_size / nsub
+    
+    xi , yi , zi  = edge(subvol_index, nsub)
+    submask = np.where((xi <cat[:, 1]) * \
+                       (cat[:, 1] < xi + subbox_size) * \
+                       (yi <cat[:, 2]) * \
+                       (cat[:, 2] < yi + subbox_size) * \
+                       (zi <cat[:, 3]) *  \
+                       (cat[:, 3] < zi + subbox_size))
+    
+    return cat[submask]
+
+def mask_positions(pos , subvol_index , nsub):
+    '''masks the positions of galaxies in
+       AM catalogs'''
+
     box_size = 250.
     subbox_size = 1.*box_size / nsub
     
@@ -117,21 +136,28 @@ def mask_galaxy_table(pos , subvol_index , nsub):
                        (zi <pos[:, 2]) *  \
                        (pos[:, 2] < zi + subbox_size))
     
-    return submask
+    return pos[submask] - edge(subvol_index,nsub)
 
 def compute_jackknife_covariance(Mr , nsub):
     
-    filename = util.dat_dir+'AM_Mr'+str(Mr)+'dat'
+    box_size = 250.
+    filename = util.dat_dir()+'AM_Mr'+str(Mr)+'dat'
     cat = np.loadtxt(filename)
     pos = cat[:,1:4]  #positions of all galaxies in the box
+    vel = cat[:,4:7]  #velocities of all galaxies
+    
+    x , y , z = pos[:,0], pos[:,1], pos[:,2]
+    vx, vy, vz = vel[:,0], vel[:,1], vel[:,2]
+    rsd_pos = return_xyz_formatted_array(x, y, z, velocity = vz, velocity_distortion_dimension = 'z')
+    rsd_pos = enforce_periodicity_of_box(rsd_pos, box_size)
+    
     number_of_subboxes = nsub ** 3
    
     #Corrfunc settings for wp measurements:
     
-    box_size = 250.
     subbox_size = box_size / nsub
     nthreads = 4
-    pimax = 40.0
+    pimax = 25.0
     binfile = path.join(path.dirname(path.abspath(__file__)),
                         "../", "bin")
     len_wp = len(np.loadtxt(binfile))
@@ -140,25 +166,17 @@ def compute_jackknife_covariance(Mr , nsub):
 
     for subvol_index in xrange(number_of_subboxes):
 
-        mask = mask_galaxy_table(pos , subvol_index , nsub)
-        sub_cat = cat[mask]
-        
-        x, y, z = cat[:,1:4]
-        vx, vy, vz = cat[:,4:7]
-        #applying RSD:
-        rsd_pos = return_xyz_formatted_array(x, y, z, velocity = vz, velocity_distortion_dimension = 'z')
-        #enforcing periodic boundary conditions:
-        rsd_pos = enforce_periodicity_of_box(rsd_pos, self.boxsize)
-        rsd_pos = rsd_pos.astype(np.float32)
-        x, y, z = rsd_pos[:,0] , rsd_pos[:,1] , rsd_pos[:,2]
-        wp_result = _countpairs.countpairs_wp(boxsize, pimax, 
+        sub_rsd_pos = mask_positions(rsd_pos , subvol_index , nsub)
+        sub_rsd_pos = sub_rsd_pos.astype(np.float32)
+        sub_x, sub_y, sub_z = sub_rsd_pos[:,0] , sub_rsd_pos[:,1] , sub_rsd_pos[:,2]
+        wp_result = _countpairs.countpairs_wp(subbox_size, pimax, 
                                    nthreads, binfile, 
-				   x, y, z)
-        nbars[subvol_index] = 1.*len(rsd_pos)/subbox_size**3.
-        wps[subvol_index] = np.array(results_wp)[:,3]
+				   sub_x, sub_y, sub_z)
+        nbars[subvol_index] = 1.*len(sub_rsd_pos)/subbox_size**3.
+        wps[subvol_index] = np.array(wp_result)[:,3]
            
     nbar_covar = jackknife_covariance_matrix(nbars)    
-    wp_covar = jackknife_covariance_matrix(wp)    
+    wp_covar = jackknife_covariance_matrix(wps)    
 
     return nbar_covar , wp_covar
 
@@ -176,4 +194,7 @@ def save_jackknife_covariance(Mr , nsub):
 
 if __name__ == "__main__":
 
-     impose_luminosity_threshold(19)   
+     impose_luminosity_threshold(19)
+     #save_luminosity_threshold(19)    
+     #save_nbar_clustering(19)
+     save_jackknife_covariance(19 , 3)   
